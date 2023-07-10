07f6b5c273c1b819e34e087309b06a3e84250ae2
James-Frown <21122@ctca.co.za>
2023-07-10 16:00:47 +0200

    adding ability to change name and surname or email

--- basic-app/src/app/components/settings/settings.tsx
+++ basic-app/src/app/components/settings/settings.tsx
@@ -5,6 +5,7 @@ import Logout from "../logout/logout";
 import { useEffect, useState } from "react";
 import { collection, doc, getDocs, updateDoc } from "firebase/firestore";
 import { auth, db } from "@/config/firebase";
+import { updateEmail } from "firebase/auth";
 
 export default function Settings() {
 
@@ -14,9 +15,13 @@ export default function Settings() {
 
     const [editUser, setEditUser] = useState(false);
 
+    const [editEmail, setEditEmail] = useState(false);
+
     const [newSurname, setNewSurname] = useState("");
 
     const [newName, setNewName] = useState("");
+
+    const [newEmail, setNewEmail] = useState("");
 
     const userCollectionRef = collection(db, "users");
 
@@ -44,10 +49,31 @@ export default function Settings() {
         const usersDoc = doc(db, "users", id)
         setEditUser(false);
         try {
-            await updateDoc(usersDoc, {
-                Name: newName,
-                Surname: newSurname
-            });
+            if (authUser !== null) {
+                await updateDoc(usersDoc, {
+                    Name: newName,
+                    Surname: newSurname,
+                });
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    function editUserEmail() {
+        setEditEmail(true);
+    }
+
+    const saveUserEmail = async (id) => {
+        const usersDoc = doc(db, "users", id)
+        setEditEmail(false);
+        try {
+            if (authUser !== null) {
+                await updateDoc(usersDoc, {
+                    Email: newEmail
+                });
+                await updateEmail(authUser, newEmail);
+            }
         } catch (error) {
             console.log(error);
         }
@@ -107,7 +133,13 @@ export default function Settings() {
                                             <div>
                                                 <button
                                                     onClick={editUserProfile}>
-                                                    Edit
+                                                    Edit Profile
+                                                </button>
+                                            </div>
+                                            <div>
+                                                <button
+                                                    onClick={editUserEmail}>
+                                                    Edit Email
                                                 </button>
                                             </div>
                                         </>
@@ -149,16 +181,44 @@ export default function Settings() {
                                                     Email: {user.Email}
                                                 </p>
                                             </div>
-
                                             <div>
                                                 <button
                                                     onClick={() => saveUserProfile(user.id)}>
                                                     Save
                                                 </button>
                                             </div>
-
                                         </>
                                     )
+                                } else {
+                                    if (editEmail === true) {
+                                        return (
+                                            <>
+                                                <div>
+                                                    <p>
+                                                        Total Points: {user.TotalPoints}
+                                                    </p>
+                                                    <p>
+                                                        Name: {user.Name}
+                                                    </p>
+                                                    <p>
+                                                        Surname: {user.Surname}
+                                                    </p>
+                                                    <input
+                                                        placeholder={user.Email}
+                                                        onChange={
+                                                            (e) => setNewEmail(e.target.value)
+                                                        }
+                                                    />
+                                                </div>
+                                                <div>
+                                                    <button
+                                                        onClick={() => saveUserEmail(user.id)}>
+                                                        Save
+                                                    </button>
+                                                </div>
+                                            </>
+                                        )
+                                    }
                                 }
                             }
                         }
