1ef4e9ceab28907140d28dec7b9f4c26e9b5ed9d
James-Frown <21122@ctca.co.za>
2023-07-10 15:43:43 +0200

    latest chnages | made the db update the users name and surname if users wish to edit their details!

--- basic-app/src/app/components/settings/settings.tsx
+++ basic-app/src/app/components/settings/settings.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import Logout from "../logout/logout";
 import { useEffect, useState } from "react";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, doc, getDocs, updateDoc } from "firebase/firestore";
 import { auth, db } from "@/config/firebase";
 
 export default function Settings() {
@@ -11,6 +11,12 @@ export default function Settings() {
     const [usersList, setUsersList] = useState([] as any);
 
     const authUser = auth.currentUser;
+
+    const [editUser, setEditUser] = useState(false);
+
+    const [newSurname, setNewSurname] = useState("");
+
+    const [newName, setNewName] = useState("");
 
     const userCollectionRef = collection(db, "users");
 
@@ -29,6 +35,23 @@ export default function Settings() {
             console.log(err);
         }
     };
+
+    function editUserProfile() {
+        setEditUser(true);
+    }
+
+    const saveUserProfile = async (id) => {
+        const usersDoc = doc(db, "users", id)
+        setEditUser(false);
+        try {
+            await updateDoc(usersDoc, {
+                Name: newName,
+                Surname: newSurname
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
     useEffect(() => {
         getUsersList();
@@ -52,36 +75,94 @@ export default function Settings() {
                 </div>
                 <div>
                     {usersList.map((user) => {
-                        if (authUser === null) {
-                            return (
-                                <>
-                                    <div>
-                                        <h1>
-                                            User has no auth!
-                                        </h1>
-                                    </div>
-                                </>
-                            )
-                        } else {
-                            if (authUser.uid === user.UID) {
+                        if (editUser === false) {
+                            if (authUser === null) {
                                 return (
                                     <>
-                                        <p>
-                                            Name: {user.Name}
-                                        </p>
-                                        <p>
-                                            Surname: {user.Surname}
-                                        </p>
-                                        <p>
-                                            Email: {user.Email}
-                                        </p>
-                                        <p>
-                                            Total Points: {user.TotalPoints}
-                                        </p>
+                                        <div>
+                                            <h1>
+                                                User has no auth!
+                                            </h1>
+                                        </div>
                                     </>
                                 )
+                            } else {
+                                if (authUser.uid === user.UID) {
+                                    return (
+                                        <>
+                                            <div>
+                                                <p>
+                                                    Total Points: {user.TotalPoints}
+                                                </p>
+                                                <p>
+                                                    Name: {user.Name}
+                                                </p>
+                                                <p>
+                                                    Surname: {user.Surname}
+                                                </p>
+                                                <p>
+                                                    Email: {user.Email}
+                                                </p>
+                                            </div>
+                                            <div>
+                                                <button
+                                                    onClick={editUserProfile}>
+                                                    Edit
+                                                </button>
+                                            </div>
+                                        </>
+                                    )
+                                }
+                            }
+                        } else {
+                            if (authUser === null) {
+                                return (
+                                    <>
+                                        <div>
+                                            <h1>
+                                                User has no auth!
+                                            </h1>
+                                        </div>
+                                    </>
+                                )
+                            } else {
+                                if (authUser.uid === user.UID) {
+                                    return (
+                                        <>
+                                            <div>
+                                                <p>
+                                                    Total Points: {user.TotalPoints}
+                                                </p>
+                                                <input
+                                                    placeholder={user.Name}
+                                                    onChange={
+                                                        (e) => setNewName(e.target.value)
+                                                    }
+                                                />
+                                                <input
+                                                    placeholder={user.Surname}
+                                                    onChange={
+                                                        (e) => setNewSurname(e.target.value)
+                                                    }
+                                                />
+                                                <p>
+                                                    Email: {user.Email}
+                                                </p>
+                                            </div>
+
+                                            <div>
+                                                <button
+                                                    onClick={() => saveUserProfile(user.id)}>
+                                                    Save
+                                                </button>
+                                            </div>
+
+                                        </>
+                                    )
+                                }
                             }
                         }
+
                     })}
                 </div>
             </div>
